84cdc8b7757026021b77dc460f84d731733701d1
Daryl <daryl@MOBOs-MacBook-Pro.local>
2018-11-20 06:31:10 -0800

    Added functionality for next and previous buttons

--- builds/angular/js/controllers.js
+++ builds/angular/js/controllers.js
@@ -28,6 +28,26 @@ myControllers.controller('DetailsController',
 
         $scope.whichItem = $routeParams.itemId;
 
+        if ($routeParams.itemId > 0) {
+
+          $scope.prevItem = Number($routeParams.itemId) - 1;
+
+        } else {
+
+          $scope.prevItem = $scope.artists.length - 1;
+
+        }
+
+        if ($routeParams.itemId < $scope.artists.length - 1) {
+
+          $scope.nextItem = Number($routeParams.itemId) + 1;
+
+        } else {
+
+          $scope.nextItem = 0;
+
+        }
+
       });
 
 });
